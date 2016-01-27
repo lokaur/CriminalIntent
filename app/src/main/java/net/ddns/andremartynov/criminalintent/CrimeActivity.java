5694552ffa1622de18a3164f52d85e92a0e16f78
Andrey Martynov <andremartynov@gmail.com>
2016-01-27 23:52:33 +0300

    chapter 7 done

--- app/src/main/java/net/ddns/andremartynov/criminalintent/CrimeActivity.java
+++ app/src/main/java/net/ddns/andremartynov/criminalintent/CrimeActivity.java
@@ -1,13 +1,25 @@
 package net.ddns.andremartynov.criminalintent;
 
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 
-public class CrimeActivity extends FragmentActivity {
+public class CrimeActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_crime);
+
+		FragmentManager fm = getFragmentManager();
+		Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+		if (fragment == null) {
+			fragment = new CrimeFragment();
+			fm.beginTransaction()
+					.add(R.id.fragment_container, fragment)
+					.commit();
+		}
 	}
 }
--- app/src/main/java/net/ddns/andremartynov/criminalintent/CrimeFragment.java
+++ app/src/main/java/net/ddns/andremartynov/criminalintent/CrimeFragment.java
@@ -1,8 +1,7 @@
 package net.ddns.andremartynov.criminalintent;
 
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -19,7 +18,6 @@ public class CrimeFragment extends Fragment {
 		mCrime = new Crime();
 	}
 
-	@Nullable
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_crime, container, false);
